5dc33456b6dcf99119f6bae65eb17a6863a8bfe7
Andrew Coulton <andrew@ingenerator.com>
2015-11-24 14:18:05 +0000

    Backup snapshots to s3

--- get-snapshot.sh
+++ get-snapshot.sh
@@ -6,6 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$DIR/environment.sh"
 source "$DIR/logecho.sh"
 # environment.sh defines CAMERA_HOST
+# environment.sh defines S3_BUCKET
 CAMERA_USER=ubnt
 CAMERA_KEY="$DIR/.ssh/fetcam_ubnt_rsa"
 
@@ -29,3 +30,7 @@ logecho "Done"
 
 logecho "Updating most recent snapshot"
 cp "$SNAPSHOT_FILE" "$DIR/report/last-snapshot.jpeg"
+
+logecho "Backing up to S3"
+s3cmd --config="$DIR/.s3cfg" put "$SNAPSHOT_FILE" "s3://$S3_BUCKET/snapshots/$SNAPSHOT_DATE/"
+logecho "All done"
